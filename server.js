848bb7f5e541550ee70cea8ccdd909d2cb8e8c27
Kunal Thapliyal <kunal_m240690cs@nitc.ac.in>
2026-02-20 21:46:41 +0530

    server.js

--- server.js
+++ server.js
@@ -214,8 +214,8 @@ app.get('/api/youtube/uploads', async (req, res) => {
 /* ================= CONTACT ================= */
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false,   // IMPORTANT: false for 587
   auth: {
     user: process.env.GMAIL_USER,
     pass: process.env.GMAIL_PASSWORD,
